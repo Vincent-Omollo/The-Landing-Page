1051bb5dd159dab2ad1c4c0bccf1ca7bae356dd0
Vincent Omollo <vincentokoth1290@gmail.com>
2023-02-27 09:54:59 +0300

    Add README.md

---
+++ st.js
@@ -0,0 +1,10 @@
+const googleBtn = document.querySelector('.google-btn');
+const facebookBtn = document.querySelector('.facebook-btn');
+
+googleBtn.addEventListener('click', () => {
+  // Sign up with Google
+});
+
+facebookBtn.addEventListener('click', () => {
+  // Sign up with Facebook
+});
